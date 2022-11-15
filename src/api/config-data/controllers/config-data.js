1d0534243bdd8dd1f23019872b927c2926f7b9ff
punzmax <57678080+punzmax@users.noreply.github.com>
2022-11-15 23:17:44 +0100

    Added colors to everything

--- src/api/config-data/controllers/config-data.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * config-data controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::config-data.config-data');
--- src/api/config-data/services/config-data.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * config-data service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::config-data.config-data');
