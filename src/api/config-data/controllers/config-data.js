576955753e6a58a518d62b26a66f803ad4870ec6
punzmax <57678080+punzmax@users.noreply.github.com>
2022-11-15 23:22:36 +0100

    Add files via upload

---
+++ src/api/config-data/controllers/config-data.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * config-data controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::config-data.config-data');
